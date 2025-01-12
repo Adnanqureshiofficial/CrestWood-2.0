bd6d494303475462012fdfb3b45f4c04234cd9af
Adnan Qureshi <99614298+Adnanqureshiofficial@users.noreply.github.com>
2025-01-12 13:59:14 +0000

    index.html create

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+
+'Italiana': ['Italiana', 'sans-serif'] ,
+
+      }
+    },
+  },
+  plugins: [],
+}
+
